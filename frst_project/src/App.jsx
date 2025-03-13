e5f0d5ea4346c45b17f5be3e55a8f63c2765b722
Bhanuka10 <sahanbhanuka10@gmail.com>
2025-03-13 18:58:27 +0530

    first learning

--- frst_project/src/App.jsx
+++ frst_project/src/App.jsx
@@ -1,10 +1,27 @@
 
 
 function App() {
+  const a =30;
+  const getfulname = (a,b,c)=>{
+    return `${a} ${b} ${c}`;
+  }
+  const arr=["apple","orange","grapes"];
+  const yep=<ul>
+    <li>java</li>
+    <li>java script</li>
+    <li>HTML</li>
+    <li>Css</li>
+  </ul>;
   return (
     <>
-    <div classname='cont'> 
+    <div className='cont'> 
       <h1>React App</h1>
+      <p>{20/20}% for jsx</p>
+      <p>const value is {a}</p>
+      <p>{a>50 ? "greater" : "lesser"}</p>
+      <p>full name is {getfulname("sahan","Bhanuka","Bandaranayake")}</p>
+      <p>{arr[2]}</p>
+      <p>{yep}</p>
 
     </div>
     </>
