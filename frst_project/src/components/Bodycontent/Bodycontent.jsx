894db0db3d92c2809f36e7d7255bec9601f99402
Bhanuka10 <sahanbhanuka10@gmail.com>
2025-03-14 23:52:40 +0530

    display img from src file

--- frst_project/src/components/Bodycontent/Bodycontent.jsx
+++ frst_project/src/components/Bodycontent/Bodycontent.jsx
@@ -1,16 +1,18 @@
-import React from 'react'
-import './Bodycontent.css'
+import React from 'react';
+import './Bodycontent.css';
+import reactlogo from '../../assets/react.svg'; // Correct import
 
 function Bodycontent() {
   return (
     <div id="middle">
-        <p>Middle content</p>
-        <div>
-          <img src="vite.svg" alt="vite" />
-          <img src="react.svg" alt="react logo" />
-        </div>
+      <p>Middle content</p>
+      <div>
+        {/* Correct image paths */}
+        <img src="/vite.svg" alt="vite" /> {/* Assuming vite.svg is in the public folder */}
+        <img src={reactlogo} alt="react" /> {/* Correct way to use imported image */}
       </div>
-  )
+    </div>
+  );
 }
 
-export default Bodycontent
+export default Bodycontent;
