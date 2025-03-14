3f7be43657e17483cbe5b2bf1762a9ead090e82f
Bhanuka10 <sahanbhanuka10@gmail.com>
2025-03-14 23:44:45 +0530

    show img from public folder

--- frst_project/src/components/Bodycontent/Bodycontent.jsx
+++ frst_project/src/components/Bodycontent/Bodycontent.jsx
@@ -5,6 +5,10 @@ function Bodycontent() {
   return (
     <div id="middle">
         <p>Middle content</p>
+        <div>
+          <img src="vite.svg" alt="vite" />
+          <img src="react.svg" alt="react logo" />
+        </div>
       </div>
   )
 }
