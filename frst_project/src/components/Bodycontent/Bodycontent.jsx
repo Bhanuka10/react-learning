4c684632d5efec9457215812dad134c9a272386a
Bhanuka10 <sahanbhanuka10@gmail.com>
2025-03-14 23:54:05 +0530

    new update

--- frst_project/src/components/Bodycontent/Bodycontent.jsx
+++ frst_project/src/components/Bodycontent/Bodycontent.jsx
@@ -1,18 +1,18 @@
-import React from 'react';
-import './Bodycontent.css';
-import reactlogo from '../../assets/react.svg'; // Correct import
+import React from 'react'
+import './Bodycontent.css'
+import reactlogo from "../../assets/react.svg"
 
 function Bodycontent() {
   return (
     <div id="middle">
-      <p>Middle content</p>
-      <div>
-        {/* Correct image paths */}
-        <img src="/vite.svg" alt="vite" /> {/* Assuming vite.svg is in the public folder */}
-        <img src={reactlogo} alt="react" /> {/* Correct way to use imported image */}
+        <p>Middle content</p>
+        <div>
+          <img src="vite.svg" alt="vite" />
+          <img src={reactlogo} alt="react" />
+          
+        </div>
       </div>
-    </div>
-  );
+  )
 }
 
-export default Bodycontent;
+export default Bodycontent
