ff232b87ba5c7310c79446e2561796b37b3dc27f
Bhanuka10 <sahanbhanuka10@gmail.com>
2025-03-18 21:01:38 +0530

    update in function

--- frst_project/src/App.jsx
+++ frst_project/src/App.jsx
@@ -4,6 +4,10 @@ import Headcontent from "./components/Headcontent/Headcontent"
 
 
 function App() {
+  function helo1(){
+    console.log("Hellow 1");
+  }
+  
   
   return (
     <>
@@ -11,6 +15,12 @@ function App() {
       <Headcontent />
       <Bodycontent />
       <Footercontent />
+    </div>
+    <div>
+      <button onClick={helo1}>Click 1</button>
+      <button>Click 2</button>
+      <button>Click 3</button>
+      <button>Click 4</button>
     </div>
   
     
