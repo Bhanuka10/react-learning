3e2df6fed8b8231a93f3fc1958c0059b945118a7
Bhanuka10 <sahanbhanuka10@gmail.com>
2025-04-05 16:04:29 +0530

    FIRST if  else

--- frst_project/src/App.jsx
+++ frst_project/src/App.jsx
@@ -7,6 +7,8 @@ function App() {
   function helo1(){
     console.log("Hellow 1");
   }
+  let data = "girl";
+  
   
   
   return (
@@ -16,6 +18,10 @@ function App() {
       <Bodycontent />
       <Footercontent />
     </div>
+    <div>
+    {data == "gi"?
+    <h1>girl</h1>:<h1>boy</h1>}
+  </div>
     <div>
       <button onClick={helo1}>Click 1</button>
       <button>Click 2</button>
